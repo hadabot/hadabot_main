c2fc32ab8df3423878dcf186b2ebc8774412de64
Jack Pien <jack@hadabot.com>
2020-04-23 14:26:35 -0700

    Some odometry code

--- content/p6/hadabot_ws/src/hadabot_bringup/src/hadabot_driver.cpp
+++ content/p6/hadabot_ws/src/hadabot_bringup/src/hadabot_driver.cpp
@@ -7,30 +7,49 @@
 using std::placeholders::_1;
 
 typedef enum {
-  HB_LEFT,
-  HB_RIGHT
+  LEFT,
+  RIGHT
 } HBSide;
 
 class HadabotDriver : public rclcpp::Node
 {
   private:
 
-  void wheel_radps_cb(const std_msgs::msg::Float32::SharedPtr msg)
-  {
-    RCLCPP_INFO(this->get_logger(), "I heard: '%f'", msg->data);
-  }
+    void wheel_radps(
+      const std_msgs::msg::Float32::SharedPtr msg, HBSide which_side)
+    {
+      std::string the_side = which_side == HBSide::LEFT ? "left" : "right";
+      RCLCPP_INFO(
+        this->get_logger(), "%s side: '%f'", the_side.c_str(), msg->data);
+    }
 
-  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr radps_left_sub_;
+    void wheel_radps_left_cb(const std_msgs::msg::Float32::SharedPtr msg)
+    {
+      this->wheel_radps(msg, HBSide::LEFT);
+    }
+
+    void wheel_radps_right_cb(const std_msgs::msg::Float32::SharedPtr msg)
+    {
+      this->wheel_radps(msg, HBSide::RIGHT);
+    }
+
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr radps_left_sub_;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr radps_right_sub_;
 
   public:
 
-  HadabotDriver() : Node("hadabot_driver") 
-  {
-    radps_left_sub_ = this->create_subscription<std_msgs::msg::Float32>(
-      "/hadabot/wheel_radps_left", 10,
-      std::bind(&HadabotDriver::wheel_radps_cb, this, _1)
-    );
-  }
+    HadabotDriver() : Node("hadabot_driver") 
+    {
+      radps_left_sub_ = this->create_subscription<std_msgs::msg::Float32>(
+        "/hadabot/wheel_radps_left", 10,
+        std::bind(&HadabotDriver::wheel_radps_left_cb, this, _1)
+      );
+
+      radps_right_sub_ = this->create_subscription<std_msgs::msg::Float32>(
+        "/hadabot/wheel_radps_right", 10,
+        std::bind(&HadabotDriver::wheel_radps_right_cb, this, _1)
+      );
+    }
 };
 
 int main(int argc, char ** argv)
