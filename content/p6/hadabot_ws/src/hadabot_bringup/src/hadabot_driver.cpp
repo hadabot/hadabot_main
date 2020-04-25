4758436f7cd5c4e1443fbca777a18b49713dee16
Jack Pien <jack@hadabot.com>
2020-04-25 08:14:35 -0700

    Some driver ROS code. Make sure user and group id used in container match that of the host so file permissions don't get messed up.

--- content/p6/hadabot_ws/src/hadabot_bringup/src/hadabot_driver.cpp
+++ content/p6/hadabot_ws/src/hadabot_bringup/src/hadabot_driver.cpp
@@ -1,15 +1,21 @@
 #include <cstdio>
+#include <chrono>
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float32.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 
+using namespace std::chrono_literals;
 using std::placeholders::_1;
 
 typedef enum {
   LEFT,
   RIGHT
 } HBSide;
+
+#define UPDATE_DT 100ms
+#define PUBLISH_DT 500ms
 
 class HadabotDriver : public rclcpp::Node
 {
@@ -33,12 +39,36 @@ class HadabotDriver : public rclcpp::Node
       this->wheel_radps(msg, HBSide::RIGHT);
     }
 
+    void update_odometry() 
+    {
+      // Get the time delta since last update
+      //int dt_ms;
+
+      // Compute distance traveled for each wheel
+      //float d_left_m;
+      //float d_right_m;
+
+    }
+
+    void publish_odometry()
+    {
+      rclcpp::Clock clk = rclcpp::Clock();
+      double t = clk.now().seconds();
+      RCLCPP_INFO(this->get_logger(), "Time: %f", t); 	
+    }
+
+    rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr odometry_pub_;
     rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr radps_left_sub_;
     rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr radps_right_sub_;
+    rclcpp::TimerBase::SharedPtr update_odometry_timer_;
+    rclcpp::TimerBase::SharedPtr publish_odometry_timer_;
+
+    float wheel_radius_m_;
+    float wheelbase_m_;
 
   public:
 
-    HadabotDriver() : Node("hadabot_driver") 
+    HadabotDriver() : Node("hadabot_driver"), wheel_radius_m_(0.035), wheelbase_m_(0.14)
     {
       radps_left_sub_ = this->create_subscription<std_msgs::msg::Float32>(
         "/hadabot/wheel_radps_left", 10,
@@ -49,6 +79,13 @@ class HadabotDriver : public rclcpp::Node
         "/hadabot/wheel_radps_right", 10,
         std::bind(&HadabotDriver::wheel_radps_right_cb, this, _1)
       );
+
+      odometry_pub_ = this->create_publisher<nav_msgs::msg::Odometry>(
+        
+      )
+
+      publish_odometry_timer_ = this->create_wall_timer(
+        PUBLISH_DT, std::bind(&HadabotDriver::publish_odometry, this));
     }
 };
 
