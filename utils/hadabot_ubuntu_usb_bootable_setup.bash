c1cfe7b2bda0360dd717956819ec49c961db9214
Jack Pien <jack@hadabot.com>
2020-10-26 23:33:27 +0000

    Update hadabot_ubuntu_usb_bootable_setup.bash
    Additional bash code to setup 20.04 bootable USB for Hadabot dev

--- utils/hadabot_ubuntu_usb_bootable_setup.bash
+++ utils/hadabot_ubuntu_usb_bootable_setup.bash
@@ -1,3 +1,7 @@
+#! /bin/bash
+
+# With a terminal from Ubuntu, run "wget -O hadabot_ubuntu_usb_bootable_setup.bash https://raw.githubusercontent.com/hadabot/hadabot_main/master/utils/hadabot_ubuntu_usb_bootable_setup.bash"
+
 sudo add-apt-repository universe
 sudo apt-get update
 
@@ -14,7 +18,7 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker ubuntu
 
 # Install docker-compose
@@ -25,8 +29,30 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo apt-get install -y python3-pip
 
 # Install the tools needed to flash the ESP32
+echo "export PATH=$PATH:~/.local/bin" >> ~/.bashrc
 pip3 install -U esptool adafruit-ampy
+wget -O esp32-micropython.bin https://micropython.org/resources/firmware/esp32-idf3-20200902-v1.13.bin
+sudo usermod -aG dialout ubuntu
 
 # Clone the hadabot repo
 git clone https://github.com/hadabot/hadabot_main.git
+
+# Login again to another shell to activate dialout and docker permission group
+su ubuntu
+
+# Build the hadabot containers
+cd hadabot_main/docker
+echo "We will be building the Hadabot Docker containers. This may take around 30 min or so depending on your network speed..."
+docker-compose up -d
+
+# Instructions on how to get the ESP32 flashed
+cd ~
+echo "You are now read to flash the ESP32."
+echo "Connect your ESP32 with a quality micro-USB"
+echo "Type the following in your terminal below - one command at a time:"
+echo ""
+echo "$ cd hadabot_main/content/p7/firmware"
+echo "$ esptool.py --port /dev/ttyUSB0 erase_flash"
+echo "$ esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 460800 write_flash -z 0x1000 ~/esp32-micropython.bin"
+echo "$ ampy --port /dev/ttyUSB0 run clean_firmware.py"
 
