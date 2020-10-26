757789f47a55199e1c185c8d8636b27cee873c1f
Jack Pien <jack@hadabot.com>
2020-10-26 23:08:18 +0000

    Create hadabot_ubuntu_usb_bootable_setup.bash
    Add bash script to set up a 20.04 LTS "Try Ubuntu" OS ISO booted from a USB stick.

---
+++ utils/hadabot_ubuntu_usb_bootable_setup.bash
@@ -0,0 +1,32 @@
+sudo add-apt-repository universe
+sudo apt-get update
+
+# Install docker
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker ubuntu
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Install necessary python components
+sudo apt-get install -y python3-pip
+
+# Install the tools needed to flash the ESP32
+pip3 install -U esptool adafruit-ampy
+
+# Clone the hadabot repo
+git clone https://github.com/hadabot/hadabot_main.git
+
