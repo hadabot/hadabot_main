d700517e4b82debcd2e4433fc9e51de3e1f60616
Jack Pien <jack@hadabot.com>
2020-10-28 17:51:38 -0700

    Update hadabot_ubuntu_usb_bootable_setup.bash
    Update the setup script to adjust to Ubuntu installed on a USB thumb drive (vs the USB "Try Ubuntu" option).

--- utils/hadabot_ubuntu_usb_bootable_setup.bash
+++ utils/hadabot_ubuntu_usb_bootable_setup.bash
@@ -2,16 +2,17 @@
 
 # With a terminal from Ubuntu, run "wget -O hadabot_setup.bash https://raw.githubusercontent.com/hadabot/hadabot_main/master/utils/hadabot_ubuntu_usb_bootable_setup.bash"
 
-# "Try Ubuntu" mounts 4gb to file location where docker lives. So we will create a symbolic link to a different location with more storage space
-sudo mkdir /var/log/var_lib_docker
-cd /var/lib
-sudo ln -s ../log/var_lib_docker docker
-cd ~
-
+echo "-----"
+echo "Updating system first... you may need to enter your password for super user access to update the system."
+echo "-----"
 sudo add-apt-repository universe
 sudo apt-get update
+sudo apt-get upgrade -y
 
 # Install docker
+echo "-----"
+echo "Installing docker..."
+echo "-----"
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
@@ -25,17 +26,20 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-sudo usermod -aG docker ubuntu
+sudo usermod -aG docker ${USER}
 
 # "Try Ubuntu" does not like it when docker users aufs as the Docker storage driver
 # When using default aufs, "docker run hello-world" will cause a weird invalid arg aufs error.
-sudo cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service.orig
-sed 's/ExecStart=\/usr\/bin\/dockerd -H/ExecStart=\/usr\/bin\/dockerd --storage-driver=devicemapper -H/g' /lib/systemd/system/docker.service.orig > docker.service
-sudo mv docker.service /lib/systemd/system/docker.service
-sudo systemctl daemon-reload
-sudo systemctl restart docker.service
+#sudo cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service.orig
+#sed 's/ExecStart=\/usr\/bin\/dockerd -H/ExecStart=\/usr\/bin\/dockerd --storage-driver=devicemapper -H/g' /lib/systemd/system/docker.service.orig > docker.service
+#sudo mv docker.service /lib/systemd/system/docker.service
+#sudo systemctl daemon-reload
+#sudo systemctl restart docker.service
 
 # Install docker-compose
+echo "-----"
+echo "Installing docker-compose..."
+echo "-----"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
@@ -43,10 +47,13 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo apt-get install -y python3-pip
 
 # Install the tools needed to flash the ESP32
+echo "-----"
+echo "Installing ESP32 tools..."
+echo "-----"
 echo "export PATH=$PATH:~/.local/bin" >> ~/.bashrc
 pip3 install -U esptool adafruit-ampy
 wget -O esp32-micropython.bin https://micropython.org/resources/firmware/esp32-idf3-20200902-v1.13.bin
-sudo usermod -aG dialout ubuntu
+sudo usermod -aG dialout ${USER}
 
 # Clone the hadabot repo
 git clone https://github.com/hadabot/hadabot_main.git
@@ -54,14 +61,16 @@ git clone https://github.com/hadabot/hadabot_main.git
 echo ""
 echo "------------------"
 echo ""
-echo "We are ready for step 2 of the Hadabot Bootable USB setup."
+echo "OK, READ CAREFULLY!! We are ready for step 2 of the Hadabot Bootable USB setup."
 echo ""
-echo "1. Using a quality micro-USB cable, connect your ESP32 to your computer."
+echo "1. Enter your password again to login to a new shell so you can run Docker and access the ESP32 device."
 echo ""
-echo "2. Once you have the ESP32 connected, execute the next setup script by typing the command:"
+echo "2. Using a quality micro-USB cable, connect your ESP32 to your computer."
+echo ""
+echo "3. Once you have the ESP32 connected, execute the next setup script by typing the command:"
 echo ""
 echo "    $ bash hadabot_main/utils/hadabot_ubuntu_bootable_usb_setup_part_02.bash"
 echo ""
 
 # Login to another shell to activate dialout and docker permission group
-su ubuntu
+su ${USER}
