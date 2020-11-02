66fd3c6dd43d087a1893ede670cb9d548243181d
Jack Pien <jack@hadabot.com>
2020-11-02 14:23:32 -0800

    Split the main script to set up Ubuntu USB

--- utils/hadabot_ubuntu_bootable_usb_setup.bash
+++ utils/hadabot_ubuntu_bootable_usb_setup.bash
@@ -43,52 +43,12 @@ echo "-----"
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Install necessary python components
-sudo apt-get install -y python3-pip
-
-# Install the tools needed to flash the ESP32
-echo "-----"
-echo "Installing ESP32 tools..."
-echo "-----"
-echo "export PATH=$PATH:${HOME}/.local/bin" >> ~/.bashrc
-export PATH=${PATH}:${HOME}/.local/bin
-pip3 install -U esptool adafruit-ampy
-wget -O esp32-micropython.bin https://micropython.org/resources/firmware/esp32-idf3-20200902-v1.13.bin
-sudo usermod -aG dialout ${USER}
-sudo chmod a+rw /dev/ttyUSB0
-
-# Clone the hadabot repo
-git clone https://github.com/hadabot/hadabot_main.git
-
-echo ""
-echo "------------------"
-echo ""
-echo "OK, READ CAREFULLY!! We are ready for step 2 of the Hadabot Bootable USB setup."
-echo ""
-echo "1. Enter your password again to login to a new shell so you can run Docker and access the ESP32 device."
-echo ""
-echo "2. Using a quality micro-USB cable, connect your ESP32 to your computer."
-echo ""
-echo "3. Once you have the ESP32 connected, execute the next setup script by typing the command:"
-echo ""
-echo "    $ bash hadabot_main/utils/hadabot_ubuntu_bootable_usb_setup_part_02.bash"
-echo ""
-
 # Build the hadabot containers
 cd hadabot_main/docker
 echo "------"
 echo "We will be building the Hadabot Docker containers. This may take around 30 min or so depending on your network speed..."
 echo "------"
 sudo docker-compose up -d
-
-# Instructions on how to get the ESP32 flashed
-cd ~
-cd hadabot_main/content/p7/firmware
-esptool.py --port /dev/ttyUSB0 erase_flash
-esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 460800 write_flash -z 0x1000 ~/esp32-micropython.bin
-ampy --port /dev/ttyUSB0 run clean_firmware.py
-ampy --port /dev/ttyUSB0 put uhadabot
-#ampy --port /dev/ttyUSB0 put boot.py
 
 # Open teleop instructions
 firefox https://www.hadabot.com/ros2-hadabot-teleop.html?step=compile-unicycle-ros2-code
