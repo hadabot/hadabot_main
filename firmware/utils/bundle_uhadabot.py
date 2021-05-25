dd71356ab6c2ff885cc8ce71813c71c6fc140ae9
Jack Pien <jack@hadabot.com>
2021-05-24 20:04:07 -0700

    Script to bundle ESP32 uPy files

--- firmware/utils/bundle_uhadabot.py
+++ firmware/utils/bundle_uhadabot.py
@@ -1,6 +1,7 @@
 import zlib
 import tarfile
 import os.path
+import os
 
 def zip_file(in_file, to_f):
     f = open(in_file, "rb")
@@ -13,13 +14,14 @@ def zip_file(in_file, to_f):
     f.close()
 
 
-
 def make_tarfile(output_filename, source_dir):
     with tarfile.open(output_filename, "w") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
 
-    zip_file(output_filename, f"{output_filename}.zlib")
+    hbz_fn = output_filename.split(".")[0]
+    zip_file(output_filename, f"{hbz_fn}.hbz")
+    os.remove(output_filename)
+
 
 if __name__ == "__main__":
     make_tarfile("uhadabot.tar", "uhadabot")
-
